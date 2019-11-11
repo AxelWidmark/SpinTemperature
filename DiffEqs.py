831a2768a2c96394f11e04ee98ad4ab8dde992c9
AxelWidmark <axel.widmark@fysik.su.se>
2019-11-11 12:00:08 +0100

    Update DiffEqs.py

--- DiffEqs.py
+++ DiffEqs.py
@@ -1,4 +1,3 @@
-#!/Users/axelwidmark/anaconda/bin/python
 import math
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,48 +13,47 @@ H_mass = 0.9388 # in GeV
 
 from CrossSection import integration_over_MB,integration_over_MB_times_Ek
 
+# scattering rate for hydrogen-hydrogen (see Eq. 9 in the paper)
 def kHH10(z): # result is in units cm^3/s
     return 3.1e-11*T_gas(z)**0.357*np.exp(-32./T_gas(z))
 
+# scattering rate for hydrogen with free electron (see Eq. 9 in the paper)
 def keH10(z): # result is in units cm^3/s
     return np.exp(   -9.607+0.5*np.log(T_gas(z))*np.exp(-np.log(T_gas(z))**4.5/1800.)   )
 
+# CMB temperature as function of redshift
 def T_CMB(z):
     return 2.726*(1.+z)
 
+# hydrogen gas temperature as function of redshift, assuming adiabatic cooling
 def T_gas(z):
     return (1.+z)**2./201.**2.*T_CMB(200.)*(z<200.)+T_CMB(z)*(z>=200.)+(z<20.)*(np.exp(20.-z)-1.)/100.
 
+# effective temperature of dark matter-hydrogen interactions, see section 2.3 in the paper
 def T_tilde(z,mX):
     return T_gas(z)/H_mass/(1./mX+1./H_mass)
 
+# the hydrogen number denstiy
 def hydrogen_number_density(z): # result is in units cm^-3
-    # 0.75*0.0486*rho_c/(hydrogen mass)
     return 1.877e-7*(1.+z)**3.
 
+# the dark matter number denstiy
 def DM_number_density(z,mX): # mX in GeV please; result is in units cm^-3
     # 0.2589*rho_c
     return 1.252e-6/mX*(1.+z)**3.
 
+# fraction of free electrons
 def e_fraction(z): # units K; redshift
     T_ionize = 1.579e5 # ionization energy in Kelvin
     n = hydrogen_number_density(z)/0.75*(0.75+0.125)
     return np.sqrt(  1./n*(1.79987e10*T_gas(z))**1.5*np.exp(-T_ionize/T_gas(z))  )
 
-def x_c(z):
-    return (kHH10(z)+e_fraction(z)*keH10(z))*hydrogen_number_density(z)*0.068/T_CMB(z)/A10
-
-def x_alpha(z):
-    # is x_alpha <= 1 always? NO! alpha can go higher and will at z<15
-    # S_alpha is <= 1 however
-    return 0.
-
+# the spin temperature
 def T_spin(z,mX,mphi,sigma_0,DMfraction=1.,Ttilde=None):
     if Ttilde==None:
         Ttilde = T_tilde(z,mX)
     p10_collision = (kHH10(z)+e_fraction(z)*keH10(z))*hydrogen_number_density(z)
     p10_alpha = A10/T_hf*(z<17.)*(np.exp(17.-z)-1.)**3./200.
-    #p10_alpha = 0.
     p10_DM = DMfraction*DM_number_density(z,mX)*sigma_0*integration_over_MB(Ttilde,mX,mphi,'deexcitation')
     p01_DM = DMfraction*DM_number_density(z,mX)*sigma_0*integration_over_MB(Ttilde,mX,mphi,'excitation')
     nominator = T_CMB(z)/T_hf*A10 + np.exp(-T_hf/T_gas(z))*(p10_collision+p10_alpha) + p01_DM
@@ -63,6 +61,7 @@ def T_spin(z,mX,mphi,sigma_0,DMfraction=1.,Ttilde=None):
     rhs = nominator/denominator
     return -T_hf/np.log(rhs)
 
+# the heating power of dark matter from interactions with hydrogen
 def dark_matter_heating_per_second(z,mX,mphi,g_N,g_x,Ttilde=None):# returns mean heating of dark matter particle in units GeV/s
     if Ttilde==None:
         Ttilde = T_tilde(z,mX)
@@ -71,6 +70,7 @@ def dark_matter_heating_per_second(z,mX,mphi,g_N,g_x,Ttilde=None):# returns mean
     energy_by_deexcitations_per_second = sigma_0*hydrogen_number_density(z)*integration_over_MB_times_Ek(Ttilde,mX,mphi,'deexcitation')
     return energy_by_excitations_per_second+energy_by_deexcitations_per_second
 
+# returns the coupling constant that produces a specific spin temperature at some redshift, for specific model parameters
 def g_N_limit(z,T_spin,mX,mphi,DMfraction=1.,Ttilde=None):
     if Ttilde==None:
         Ttilde = T_tilde(z,mX)
@@ -87,10 +87,12 @@ def g_N_limit(z,T_spin,mX,mphi,DMfraction=1.,Ttilde=None):
     if DMfraction<0.3:
         g_x_squared = 1.
     else:
+        # limit from Bullet Cluster
         g_x_squared = math.sqrt(1.8e-4*mX**3.)
     g_N_squared = sigma_0*4.*pi*6.5e25*(mX**2.+mphi**2.)**2./(mX**2.*g_x_squared)
     return math.sqrt(g_N_squared)
 
+# returns the coupling constant that produces a specific spin temperature at redshift z=17, also including heating of the different gases
 def g_N_limit_iteration_z17(T_spin,mX,mphi,DMfraction=1.,Tx_init_at_z0=0.):
     Ttilde17 = T_tilde(17.,mX)
     for iteration in range(20):
@@ -120,41 +122,3 @@ def g_N_limit_iteration_z17(T_spin,mX,mphi,DMfraction=1.,Tx_init_at_z0=0.):
         Ttilde_vec.append(Ttilde)
         Tx_vec.append(Tx)
     return g_N,z_vec,Ttilde_vec,Tx_vec
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def temperatures_plot(mX,mphi,sigmas,ax,xlabel=True):
-    zvec = np.logspace(1.,3.,51.)
-    ax.loglog(zvec,T_CMB(zvec),'b',label='CMB')
-    ax.loglog(zvec,[T_gas(z) for z in zvec],'r',label='Hydrogen gas')
-    for sigma in sigmas:
-        ax.loglog(zvec,[T_spin(z,mX,mphi,sigma) for z in zvec],'k--')
-    ax.plot([],[],'k--',label='Spin gas')
-    ax.set_ylim([4e-1,4e3])
-    #ax.set_title('$m_\chi=$'+str(1e3*mX)+' MeV; $m_V=$'+str(1e9*mphi)+' eV; $\sigma\in$'+str(sigmas[0])+' -- '+str(sigmas[-1]))
-    info_1 = '$\sigma\in$'+str(sigmas[0])+' -- '+str(sigmas[-1])+' cm^2'
-    info_2 = '$m_\chi=$'+str(1e3*mX)+' MeV'
-    info_3 = '$m_V=$'+str(1e9*mphi)+' eV'
-    ax.text(1e1,10.**3.2,info_1)
-    ax.text(1e1,10.**2.8,info_2)
-    ax.text(1e1,10.**2.4,info_3)
-    if xlabel:
-        ax.set_xlabel('Redshift, z')
-    else:
-        ax.set_xticks([])
-    ax.set_ylabel('Temperature (K)')
-    ax.legend(loc=4)
